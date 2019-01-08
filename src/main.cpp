29a82ede5a70a4547ca9ba4f20aa5d887b870033
3jackattack3 <3jackattack3@gmail.com>
2019-01-07 21:38:02 -0500

    got basic functionality working on the arduino mega now

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,32 @@ int horizShoulderPos;
 int vertShoulderPos;
 int elbowPos;
 
-byte horizShoulderPin = 5;
-byte vertShoulderPin = 6;
-byte elbowPin = 10;
+byte horizShoulderPin = 2;
+byte vertShoulderPin = 3;
+byte elbowPin = 4;
+
+void writeServos(int horizShoulderPos, int vertShoulderPos, int elbowPos);
 
 void setup() {
+    pinMode(horizShoulderPin, OUTPUT);
+    pinMode(vertShoulderPin, OUTPUT);
+    pinMode(elbowPin, OUTPUT);
+
     horizShoulder.attach(horizShoulderPin);
     vertShoulder.attach(vertShoulderPin);
     elbow.attach(elbowPin);
 }
 
 void loop() {
-    horizShoulderPos = 120;
-    horizShoulder.write(horizShoulderPos);
+    horizShoulderPos = 90;
+    vertShoulderPos = 90;
+    elbowPos = 90;
+    writeServos(horizShoulderPos, vertShoulderPos, elbowPos);
     delay(500);
+}
+
+void writeServos(int horizShoulderPos, int vertShoulderPos, int elbowPos){
+    horizShoulder.write(horizShoulderPos);
+    vertShoulder.write(vertShoulderPos);
+    elbow.write(elbowPos);
 }
